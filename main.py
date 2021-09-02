a5f860015a48a09a694f450652672350455d4ad0
Akhror7 <ahror.abdullayev.2000@gmail.com>
2021-09-02 00:03:18 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+"""
+Ahror
+5/08/2021
+ahror.abdullayev.2000@gmail.com
+"""
+
+
+import telebot
+from transliterate import to_latin , to_cyrillic
+TOKEN="1919061673:AAF4kcBb_NyYfqiMBlIMWLWbIVDM9L5Fs0c"
+bot = telebot.TeleBot(token=TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    username = message.from_user.username  # Bu usul bilan foydalanuvchi nomini olishimiz mumkin
+    xabar = f'Assalom alaykum, {username} Kirill-Lotin-Kirill botiga xush kelibsiz!'
+    xabar += '\nMatningizni yuboring.'
+    bot.reply_to(message, xabar)
+
+
+# matnlar uchun mas'ul funksiya
+@bot.message_handler(func=lambda msg: msg.text is not None)
+def translit(message):
+    msg = message.text
+    javob = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    bot.reply_to(message, javob(msg))
+
+
+bot.polling()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
